63d104b6a79cf84c2ffd1a485fc82d23b1372a89
Amon <kiprotichamon03@gmail.com>
2024-10-22 16:32:46 +0300

    finaly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,20 @@ import profilePic from '../assets/profile.jpg';
 const Hero = () => {
   return (
     <section className="h-screen bg-gradient-to-r from-gray-800 to-gray-600 flex flex-col justify-center items-center text-white text-center p-10 relative">
-      <div className="absolute top-5 right-5 flex space-x-4">
+      
+      {/* Button Container for Large Screens */}
+      <div className="absolute top-5 right-5 hidden md:flex space-x-4">
         <a href="#projects" className="bg-accent text-black px-4 py-2 rounded-full hover:bg-green-400 transition duration-300">Projects</a>
         <a href="#contact" className="bg-accent text-black px-4 py-2 rounded-full hover:bg-green-400 transition duration-300">Contact</a>
       </div>
+
+      {/* Button Container for Small Screens */}
+      <div className="absolute top-5 left-5 right-5 flex justify-between md:hidden space-x-4">
+        <a href="#projects" className="bg-accent text-black px-4 py-2 rounded-full hover:bg-green-400 transition duration-300">Projects</a>
+        <a href="#contact" className="bg-accent text-black px-4 py-2 rounded-full hover:bg-green-400 transition duration-300">Contact</a>
+      </div>
+
+      {/* Profile Picture */}
       <motion.img
         src={profilePic}
         alt="Profile"
@@ -16,6 +26,8 @@ const Hero = () => {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1 }}
       />
+
+      {/* Heading */}
       <motion.h1
         className="text-6xl font-bold mb-4 border-b-2 border-white pb-2"
         initial={{ opacity: 0, y: -50 }}
@@ -24,6 +36,8 @@ const Hero = () => {
       >
         Hi, I'm Amon Kiprotich
       </motion.h1>
+
+      {/* Subheading */}
       <motion.p
         className="text-2xl mt-4 max-w-xl leading-relaxed px-4 shadow-lg bg-gray-700 rounded-lg p-6"
         initial={{ opacity: 0 }}
@@ -32,6 +46,7 @@ const Hero = () => {
       >
         Software Developer
       </motion.p>
+
       <motion.p
         className="text-lg mt-2 italic"
         initial={{ opacity: 0 }}
@@ -40,6 +55,8 @@ const Hero = () => {
       >
         Crafting Innovative Solutions
       </motion.p>
+
+      {/* Call to Action */}
       <motion.a
         href="#about"
         className="mt-8 inline-block bg-accent text-black px-6 py-3 rounded-full shadow-lg hover:bg-green-400 transition duration-300"
